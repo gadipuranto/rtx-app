ef8ee2b55c8fb5c30b948c119bdfae0c65aae483
galang.adi <galang.adi@kelaspintar.id>
2023-04-17 01:33:16 +0700

    setup redux store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import productSlice from "../features/sliceProducts";
+
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
-  },
+    product : productSlice
+  }
 });
